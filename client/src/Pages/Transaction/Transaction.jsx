8df2ad6af28349a901f38c2e4bd81bbf1914747a
Santhosh1701 <santhoshkumar13101@gmail.com>
2023-06-07 19:51:56 +0530

    Transaction

--- client/src/Pages/Transaction/Transaction.jsx
+++ client/src/Pages/Transaction/Transaction.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./transaction.css";
+import { Button, Modal } from "react-bootstrap";
 // import Header from "./header";
 import axios from "axios";
 
@@ -28,6 +29,26 @@ function Transaction() {
     vanFees: 400,
     admissionFees: 100,
   });
+  const [installedTools, setInstalledTools] = useState([
+    {
+      roll_no: 1,
+      name: "siva",
+      standard: 10,
+      fatherName: "raja",
+      motherName: "amudha",
+      address: "ssss",
+    },
+    {
+      roll_no: 2,
+      name: "siva",
+      standard: 10,
+      fatherName: "aaaa",
+      motherName: "bbb",
+      address: "kkk",
+    },
+  ]);
+
+  const [showModal, setShowModal] = useState(true);
   const [pastFees, setPastFees] = useState({
     termOne: 0,
     termTwo: 0,
@@ -36,6 +57,36 @@ function Transaction() {
     vanFees: 400,
     admissionFees: 0,
   });
+
+  const handleModal = () => {
+    setShowModal(false);
+  };
+
+  const installedToolsTable = () => {
+    if (installedTools?.length > 0) {
+      return installedTools?.map((tools, index) => {
+        const { roll_no, name, standard, fatherName, motherName, address } =
+          tools;
+        return (
+          <tr key={index}>
+            <td>{roll_no}</td>
+            <td>{name}</td>
+            <td>{standard}</td>
+            <td>{fatherName}</td>
+            <td>{motherName}</td>
+            <td>{address}</td>
+            <td>Ok</td>
+          </tr>
+        );
+      });
+    } else {
+      return (
+        <tr>
+          <td>No data to display..!</td>
+        </tr>
+      );
+    }
+  };
   // const [termOne, setTermOne] = useState(1000);
   // const [termTwo, setTermTwo] = useState(1000);
   // const [termThree, setTermThree] = useState(1000);
@@ -65,125 +116,153 @@ function Transaction() {
 
   return (
     <>
-      <div className="trans">
-        <form className="search-form">
-          <label className="search-lab" style={{"marginLeft":100}}>
-            Student Id
-          </label>
-          <input className="search-inp" placeholder="Enter roll number " />
-          <label className="search-lab">Student Name</label>
-          <input className="search-inp" placeholder="Enter student name " />
-          <button className="button">Submit</button>
-        </form>
-      </div>
+      <Modal
+        show={showModal}
+        onHide={() => handleModal()}
+        dialogClassName="modal-lg"
+      >
+        <Modal.Header closeButton>
+          <h4>Student Details</h4>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="market-table">
+            <div className="table-responsive">
+              <table className="table mb-0">
+                <thead>
+                  <tr>
+                    <th>Roll No</th>
+                    <th>Name</th>
+                    <th>Standard</th>
+                    <th>Father Name</th>
+                    <th>Mother Name</th>
+                    <th>Address</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>{installedToolsTable()}</tbody>
+              </table>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
 
-      <div className="sandy">
-        <div className="fees-details">
-          <form>
-            <label className="details-lab">Year</label>
-            <select
-            className="details-inp"
-              value={selectedYear}
-              onChange={handleYearChange}
-            >
-              <option value="">Select Year</option>
-              {Array.from({ length: endYear - startYear + 1 }, (_, index) => (
-                <option key={startYear + index} value={startYear + index}>
-                  {startYear + index}
-                </option>
-              ))}
-            </select>
-            <label className="details-lab">Term Fees</label>
-            <input className="details-inp"/>
-            <label className="details-lab">Book Fees</label>
-            <input className="details-inp"/>
-            <label className="details-lab">Van Fees</label>
-            <input className="details-inp"/>
-            <label className="details-lab">Admission Fees</label>
-            <input className="details-inp"/>
-            <label className="details-lab">Total</label>
-            <input className="details-inp"/>
+      <div className="trans">
+        <div>
+          <form className="search-form">
+            <label className="search-lab" style={{ marginLeft: 100 }}>
+              Student Id
+            </label>
+            <input className="search-inp" placeholder="Enter roll number " />
+            <label className="search-lab">Student Name</label>
+            <input className="search-inp" placeholder="Enter student name " />
+            <button className="button">Submit</button>
           </form>
-          <button className="submit-btn">Submit</button>
         </div>
-        <div className="trans-tab">
-          <table className="tot-fee">
-            <thead></thead>
-            <tbody>
-              <tr>
-                <td className="tot-fee-tr"></td>
-                <td className="tot-fee-tr">Term 1</td>
-                <td className="tot-fee-tr">Term 2</td>
-                <td className="tot-fee-tr">Term 3</td>
-                <td className="tot-fee-tr">Book Fees</td>
-                <td className="tot-fee-tr">Van Fees</td>
-                <td className="tot-fee-tr">Admmision Fees</td>
-              </tr>
-            </tbody>
-          </table>
-          <table className="tot-fee">
-            <thead></thead>
-            <tbody>
-              <tr>
-                <td className="tot-fee-tr">
-                  Total Fees
-                  <span style={{ fontSize: 11 }}> (current year)</span>
-                </td>
-                <td className="tot-fee-tr">{totalFees.termOne}</td>
-                <td className="tot-fee-tr">{totalFees.termTwo}</td>
-                <td className="tot-fee-tr">{totalFees.termThree}</td>
-                <td className="tot-fee-tr">{totalFees.bookFees}</td>
-                <td className="tot-fee-tr">{totalFees.vanFees}</td>
-                <td className="tot-fee-tr">{totalFees.admissionFees}</td>
-              </tr>
-            </tbody>
-          </table>
-          <table className="tot-fee">
-            <thead></thead>
-            <tbody>
-              <tr>
-                <td className="tot-fee-tr">Paid Fees</td>
-                <td className="tot-fee-tr">{paidFees.termOne}</td>
-                <td className="tot-fee-tr">{paidFees.termTwo}</td>
-                <td className="tot-fee-tr">{paidFees.termThree}</td>
-                <td className="tot-fee-tr">{paidFees.bookFees}</td>
-                <td className="tot-fee-tr">{paidFees.vanFees}</td>
-                <td className="tot-fee-tr">{paidFees.admissionFees}</td>
-              </tr>
-            </tbody>
-          </table>
-          <div className="last-year-fees">
+
+        <div className="sandy">
+          <div className="fees-details">
+            <form>
+              <label className="details-lab">Year</label>
+              <select
+                className="details-inp"
+                value={selectedYear}
+                onChange={handleYearChange}
+              >
+                <option value="">Select Year</option>
+                {Array.from({ length: endYear - startYear + 1 }, (_, index) => (
+                  <option key={startYear + index} value={startYear + index}>
+                    {startYear + index}
+                  </option>
+                ))}
+              </select>
+              <label className="details-lab">Term Fees</label>
+              <input className="details-inp" />
+              <label className="details-lab">Book Fees</label>
+              <input className="details-inp" />
+              <label className="details-lab">Van Fees</label>
+              <input className="details-inp" />
+              <label className="details-lab">Admission Fees</label>
+              <input className="details-inp" />
+              <label className="details-lab">Total</label>
+              <input className="details-inp" />
+            </form>
+            <button className="submit-btn">Submit</button>
+          </div>
+          <div className="trans-tab">
             <table className="tot-fee">
               <thead></thead>
               <tbody>
-                <tr>
+                <tr style={{height: 68.5}}><td className="tot-fee-tr">Fees Type</td></tr>
+                <tr><td className="tot-fee-tr">Term 1</td></tr>
+                <tr><td className="tot-fee-tr">Term 2</td></tr>
+                <tr><td className="tot-fee-tr">Term 3</td></tr>
+                <tr><td className="tot-fee-tr">Book Fees</td></tr>
+                <tr><td className="tot-fee-tr">Van Fees</td></tr>
+                <tr><td className="tot-fee-tr">AdmissionFees</td></tr>
+              </tbody>
+            </table>
+            <table className="tot-fee">
+              <thead></thead>
+              <tbody>
+                <tr style={{height: 50}}>
                   <td className="tot-fee-tr">
                     Total Fees
-                    <span style={{ fontSize: 11 }}> (Last year)</span>
+                    <span style={{ fontSize: 11 }}> (current year)</span>
                   </td>
-                  <td className="tot-fee-tr">{lyTotalFees.termOne}</td>
-                  <td className="tot-fee-tr">{lyTotalFees.termTwo}</td>
-                  <td className="tot-fee-tr">{lyTotalFees.termThree}</td>
-                  <td className="tot-fee-tr">{lyTotalFees.bookFees}</td>
-                  <td className="tot-fee-tr">{lyTotalFees.vanFees}</td>
-                  <td className="tot-fee-tr">{lyTotalFees.admissionFees}</td>
                 </tr>
+                <tr><td className="tot-fee-tr">{totalFees.termOne}</td></tr>
+                <tr><td className="tot-fee-tr">{totalFees.termTwo}</td></tr>
+                <tr><td className="tot-fee-tr">{totalFees.termThree}</td></tr>
+                <tr><td className="tot-fee-tr">{totalFees.bookFees}</td></tr>
+                <tr><td className="tot-fee-tr">{totalFees.vanFees}</td></tr>
+                <tr><td className="tot-fee-tr">{totalFees.admissionFees}</td></tr>
               </tbody>
             </table>
             <table className="tot-fee">
               <thead></thead>
               <tbody>
-                <tr>
-                  <td className="tot-fee-tr">Paid Fees</td>
-                  <td className="tot-fee-tr">{pastFees.termOne}</td>
-                  <td className="tot-fee-tr">{pastFees.termTwo}</td>
-                  <td className="tot-fee-tr">{pastFees.termThree}</td>
-                  <td className="tot-fee-tr">{pastFees.bookFees}</td>
-                  <td className="tot-fee-tr">{pastFees.vanFees}</td>
-                  <td className="tot-fee-tr">{pastFees.admissionFees}</td>
-                </tr>
+                <tr style={{height: 50}}><td className="tot-fee-tr">Paid Fees
+                <span style={{ fontSize: 11 }}> (current year)</span></td></tr>
+                <tr><td className="tot-fee-tr">{paidFees.termOne}</td></tr>
+                <tr><td className="tot-fee-tr">{paidFees.termTwo}</td></tr>
+                <tr><td className="tot-fee-tr">{paidFees.termThree}</td></tr>
+                <tr><td className="tot-fee-tr">{paidFees.bookFees}</td></tr>
+                <tr><td className="tot-fee-tr">{paidFees.vanFees}</td></tr>
+                <tr><td className="tot-fee-tr">{paidFees.admissionFees}</td></tr>
               </tbody>
             </table>
+            <div className="last-year-fees">
+              <table className="tot-fee">
+                <thead></thead>
+                <tbody>
+                  <tr>
+                    <td className="tot-fee-tr">
+                      Total Fees
+                      <span style={{ fontSize: 11 }}> (Last year)</span>
+                    </td>
+                  </tr>
+                  <tr><td className="tot-fee-tr">{lyTotalFees.termOne}</td></tr>
+                  <tr><td className="tot-fee-tr">{lyTotalFees.termTwo}</td></tr>
+                  <tr><td className="tot-fee-tr">{lyTotalFees.termThree}</td></tr>
+                  <tr><td className="tot-fee-tr">{lyTotalFees.bookFees}</td></tr>
+                  <tr><td className="tot-fee-tr">{lyTotalFees.vanFees}</td></tr>
+                  <tr><td className="tot-fee-tr">{lyTotalFees.admissionFees}</td></tr>
+                </tbody>
+              </table>
+              <table className="tot-fee">
+                <thead></thead>
+                <tbody>
+                  <tr style={{height: 50}}><td className="tot-fee-tr">Paid Fees
+                  <span style={{ fontSize: 11 }}> (Last year)</span></td></tr>
+                  <tr><td className="tot-fee-tr">{pastFees.termOne}</td></tr>
+                  <tr><td className="tot-fee-tr">{pastFees.termTwo}</td></tr>
+                  <tr><td className="tot-fee-tr">{pastFees.termThree}</td></tr>
+                  <tr><td className="tot-fee-tr">{pastFees.bookFees}</td></tr>
+                  <tr><td className="tot-fee-tr">{pastFees.vanFees}</td></tr>
+                  <tr><td className="tot-fee-tr">{pastFees.admissionFees}</td></tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
