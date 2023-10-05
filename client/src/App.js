0d004499c50a1a3374350f89f2911319add71432
Bala Shanmugam <skbalarec@gmail.com>
2023-10-05 12:57:22 +0530

    ID - 707 - Comments are proper
    Issue: Improper comments
    
    Fix: Addressed all improper comments.
    
    Testing: Started the server and did basic testing.
    ID - 707 - Comments are proper

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import login and Navbar components.
 import Login from "./Pages/Login/Login";
 import Navbar from "./Pages/Common/Navbar";
 import DashBoard from "./Pages/Dashboard/dashboard";
 import CourseFeesComponent from "./Pages/Coursefees/Coursefee.jsx";
 import Fullview from "./Pages/Fullview/Fullview";
 import StudentsComponent from "./Pages/Student/StudentForm";
-// import CourseFeesComponent from "./Pages/Coursefees/Coursefee"
 import Transaction from "./Pages/Transaction/Transaction";
 import TransactionUpdate from "./Pages/UpdateTransaction/UpdateTransaction";
 
